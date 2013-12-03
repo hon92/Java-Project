94cc5522c1c4879f76ad990a1a25e930cbd17d18
Honza <Honza@Honza-Pc>
2013-12-03 21:10:27 +0100

    game loop and tick method

--- src/Controls/Dijkstra.java
+++ src/Controls/Dijkstra.java
@@ -76,7 +76,7 @@ public class Dijkstra
         g.setColor(Color.blue);
         for (ListItem l : route)
         {
-            g.fillRect(gameBoard.convertX(l.getX() * 25), gameBoard.convertY((l.getY()) * 25), 25, 25);
+            g.fillRect(gameBoard.convertX(l.getX() * 25), gameBoard.convertY((l.getY() + 1) * 25), 25, 25);
         }
         for (int i = 0; i < temp.length; i++)
         {
